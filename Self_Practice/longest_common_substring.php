5b7c919b007def5bff47bcccb1173d152d02f68a
tajulislam <tajulislam8028@gmail.com>
2019-06-13 17:22:17 +0600

    reusuabilit_code_for: give input different length's string

--- Self_Practice/longest_common_substring.php
+++ Self_Practice/longest_common_substring.php
@@ -4,22 +4,28 @@
 $stdin = fopen('php://stdin', 'r');
 $stdout = fopen('php://stdout', 'w');
 
-fscanf(STDIN,"%s",$string1);
+fscanf(STDIN,"%s\n",$str1); // take input first string
 
-echo $first_string_length=strlen($string1)."\n";
+ $len1=strlen($str1);     //  find first string length
+ //echo"length1:".$len1."\n";//print first string length
+ //echo"\n";
 
-fscanf(STDIN,"%s",$string2);
+fscanf(STDIN,"%s\n",$str2); // take input seccond string
 
-echo $second_string_length=strlen($string2)."\n"."\n";
-
-if($first_string_length>$second_string_length)
+ $len2=strlen($str2);  // find seccond string length
+ //echo "length2:".$len2."\n"; //print seccond string length
+ //echo"\n";
+// i want , according to your wish , give input  a  string that can be small length or big length but i always store
+//in  small length's string in a len1 variable. why use that type store? because i would like to looping the loop
+// according to first string size.
+if($len1>$len2)
 {
-    $temp=$first_string_length;
-
-    $first_string_length=$second_string_length;
-
-    $second_string_length=$temp;
+    $temp=$len1;
+    $len1=$len2;
+    $len2=$temp;
 }
-echo $first_string_length;
-echo $second_string_length;
+//echo "length1:".$len1."\n";
+//echo "\n";
+//echo "length2:".$len2."\n";
+
 
