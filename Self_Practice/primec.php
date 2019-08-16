cbc11eff1579aec2b323fd88eed73c7a7f06ece8
tajulislam <tajulislam8028@gmail.com>
2019-08-16 11:59:55 +0600

    added comment

--- Self_Practice/primec.php
+++ Self_Practice/primec.php
@@ -11,7 +11,10 @@ for($t=1;$t<=$tc;$t++)
     {  //echo $in[$i]."\n";
      $cnt=0;
        
-        for($j=2;$j<=($in[$i])/2;$j++)
+        for($j=2;$j<=($in[$i])/2;$j++)// bar bar vul korci <= dite .<= dite hbe karon 4 er khetre 
+        //4/2=2 sheikhetre 2<2 hoy tahole condition ta mittha hy nd cnt er man 1 baray na jar
+        //fole amdr flag er man ta 1 bariye 4 input dile bole dei 1.
+        //Mind it 4 er kotha cinta kore <= dite hbe.
       {
         if(($in[$i])%$j==0)
         {
