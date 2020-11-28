a056cfe53965a06bb50b80bdb2dd2b8ae3ecff47
tajulislam <tajulislam8028@gmail.com>
2020-11-28 21:56:46 +0600

    successfully created custom array distinct function

---
+++ Self_Practice/reusuability_code/customFunctionSameAsPhpArrayUnique.php
@@ -0,0 +1,24 @@
+<?php
+fscanf(STDIN,"%s",$str);
+echo distinctChar($str);
+function distinctChar($str)
+{
+	$k=1;
+	$distChar=$str[0];
+	for ($i=1; $i <strlen($str) ; $i++)
+	 { 
+	 	$cnt=0;
+		for ($j=0; $j <strlen($distChar); $j++)
+		 { 
+			if($str[$i]!=$distChar[$j])
+			{
+				$cnt++;
+				if($cnt==strlen($distChar))
+				{
+                   $distChar.=$str[$i];
+				}
+			}
+		 }
+	 }
+	 return $distChar;
+}
