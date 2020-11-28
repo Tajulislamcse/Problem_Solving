9f7974e4471908362f2ab0cdfd649fee1a488dcd
tajulislam <tajulislam8028@gmail.com>
2020-11-28 22:02:18 +0600

    fix indentation of php array distinct

--- Self_Practice/reusuability_code/customFunctionSameAsPhpArrayUnique.php
+++ Self_Practice/reusuability_code/customFunctionSameAsPhpArrayUnique.php
@@ -3,22 +3,22 @@ fscanf(STDIN,"%s",$str);
 echo distinctChar($str);
 function distinctChar($str)
 {
-	$k=1;
-	$distChar=$str[0];
-	for ($i=1; $i <strlen($str) ; $i++)
-	 { 
-	 	$cnt=0;
-		for ($j=0; $j <strlen($distChar); $j++)
+		$k=1;
+		$distChar=$str[0];
+		for ($i=1; $i <strlen($str) ; $i++)
 		 { 
-			if($str[$i]!=$distChar[$j])
-			{
-				$cnt++;
-				if($cnt==strlen($distChar))
+		 	$cnt=0;
+			for ($j=0; $j <strlen($distChar); $j++)
+			 { 
+				if($str[$i]!=$distChar[$j])
 				{
-                   $distChar.=$str[$i];
+					$cnt++;
+					if($cnt==strlen($distChar))
+					{
+	                   $distChar.=$str[$i];
+					}
 				}
-			}
+			 }
 		 }
-	 }
-	 return $distChar;
+  return $distChar;
 }
